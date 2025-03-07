3280350a732286be8063f515bc76aa064a8691e5
Syaifuddin <syaifuddin@qiscus.com>
2025-03-07 15:56:20 +0700

    add Dockerfile and main.go

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+
+	port := "8080"
+	fmt.Printf("Server is running on port %s...\n", port)
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
